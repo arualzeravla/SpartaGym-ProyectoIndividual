431d54221e3772f0b663526184e4e4fa6f6d8b36
arualzeravla <laura.q.alvarez@gmail.com>
2021-02-21 00:14:14 -0300

    ejs y partials

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,4 +1,4 @@
-const controller = {
+const mainController = {
     index: (req,res) => {
         return res.render ('home');
     },
@@ -9,7 +9,32 @@ const controller = {
 
     login: (req,res) => {
         return res.render ('login');
+    },
+
+    carrito: (req,res) => {
+        return res.render ('carrito');
+    },
+
+    leonidas: (req,res) => {
+        return res.render ('leonidas');
+    },
+
+    helena: (req,res) => {
+        return res.render ('helena');
+    },
+
+    anaxandro: (req,res) => {
+        return res.render ('anaxandro');
+    },
+
+    speedRope: (req,res) => {
+        return res.render ('speedRope');
+    },
+
+    bandas: (req,res) => {
+        return res.render ('bandas');
     }
+    
 }
 
-module.exports = controller;
+module.exports = mainController;
--- routes/main.js
+++ routes/main.js
@@ -9,4 +9,16 @@ router.get ('/register', mainController.register);
 
 router.get ('/login', mainController.login);
 
+router.get ('/carrito', mainController.carrito);
+
+router.get ('/leonidas', mainController.leonidas);
+
+router.get ('/helena', mainController.helena);
+
+router.get ('/anaxandro', mainController.anaxandro);
+
+router.get ('/bandas', mainController.bandas);
+
+router.get ('/speedrope', mainController.speedRope);
+
 module.exports = router;
